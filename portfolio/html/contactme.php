f613274971fd30b61fa5969ea6d60ec8039b8536
Rasel Hossain <hossain.rasel2002@gmail.com>
2026-04-24 20:40:24 +0600

    php add

--- portfolio/html/contactme.php
+++ portfolio/html/contactme.php
@@ -17,18 +17,18 @@
       </ul>
     </nav>
     <br>
-    <form method="post">
+    <form action="contactme.php" method="post">
       <fieldset>
         <legend>Registration Form</legend>
 
         <table>
           <tr>
-            <td><label for="name">First Name:</label></td>
-            <td><input type="text" id="name" name="name" required /></td>
+            <td><label for="fname">First Name:</label></td>
+            <td><input type="text" id="fname" name="fname" required /></td>
           </tr>
           <tr>
-            <td><label for="name">Last Name:</label></td>
-            <td><input type="text" id="name" name="name" required /></td>
+            <td><label for="lname">Last Name:</label></td>
+            <td><input type="text" id="lname" name="lname" required /></td>
           </tr>
           <tr>
             <td><label>Gender:</label></td>
@@ -46,26 +46,26 @@
             <td>
               <input
                 type="radio"
-                name="Reason Of Contract"
+                name="Projects"
                 value="Projects"
               />Projects
               <input
                 type="radio"
-                name="Reason Of Contract"
+                name="Thesis"
                 value="Thesis"
               />Thesis
-              <input type="radio" name="Reason Of Contract" value="Job" />Job
+              <input type="radio" name="Job" value="Job" />Job
             </td>
           </tr>
 
           <tr>
             <td><label>Topics:</label></td>
             <td>
-              <input type="checkbox" value="Web Development" required /> Web
+              <input type="checkbox" name="web" value="Web Development" required /> Web
               Development
-              <input type="checkbox" value="Mobile Development" /> Mobile
+              <input type="checkbox" name="mobile" value="Mobile Development" /> Mobile
               Development
-              <input type="checkbox" value="AI/ML Development" /> AI/ML
+              <input type="checkbox" name="ai" value="AI/ML Development" /> AI/ML
               Development
             </td>
           </tr>
@@ -79,7 +79,7 @@
           <tr>
             <td></td>
             <td>
-              <input type="submit" value="Register" />
+              <input type="submit" name="confirm" value="confirm" />
               <input type="reset" />
             </td>
           </tr>
@@ -98,3 +98,39 @@
     </footer>
   </body>
 </html>
+
+<?php
+if (isset($_POST['confirm'])) 
+  {
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $email = $_POST['email'];
+    $gender = $_POST['gender'];
+    $consultationDate = $_POST['Consultation-date'];
+    $reasonOfContract = $_POST['Projects'] ?? $_POST['Thesis'] ?? $_POST['Job'] ?? 'Not specified';
+    $topics = [];
+
+    if (isset($_POST['web'])) {
+        $topics[] = $_POST['web'];
+    }
+    if (isset($_POST['mobile'])) {
+        $topics[] = $_POST['mobile'];
+    }
+    if (isset($_POST['ai'])) {
+        $topics[] = $_POST['ai'];
+    }
+
+    if (empty($fname) || empty($lname) || empty($gender) || empty($email) || empty($consultationDate) || empty($reasonOfContract) || empty($topics)) 
+      {
+        echo "Please fill in all required fields.";
+    } else {
+        echo "First Name: {$fname} <br>";
+        echo "Last Name: {$lname} <br>";
+        echo "Gender: {$gender}<br>";
+        echo "Email: {$email} <br>";      
+        echo "Reason of Contract: {$reasonOfContract} <br>";
+        echo "Topics: { implode(", ", $topics) } <br>";
+        echo "Consultation Date: {$consultationDate} <br>";
+    }
+ }
+?>
